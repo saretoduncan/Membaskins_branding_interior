31ff5561a23645571658aec6e929846eab61b1d6
Duncan Moiyo <duncan.moiyo@gmail.com>
2025-11-02 05:44:11 +0300

    add call to action ui and remove unnecessary imports

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,3 +1,4 @@
+import CtaComponent from "@/components/custom_ui/common/ctaComponent";
 import AboutHeroSection from "./aboutHeroSection";
 import Core_values_section from "./core_values_section";
 import Mission_cores_section from "./mission_cores_section";
@@ -8,6 +9,7 @@ const AboutPage = () => {
       <AboutHeroSection />
       <Mission_cores_section/>
       <Core_values_section/>
+      <CtaComponent/>
     </>
   );
 };
